080b31463998c2de8fd484bfe889a932eea308bc
Xavi Moll <xmollv@gmail.com>
2020-12-23 16:54:06 +0100

    Added the Plugin framework

---
+++ MWLottiePlugin/MWLottiePlugin/MWLottiePlugin.h
@@ -0,0 +1,18 @@
+//
+//  MWLottiePlugin.h
+//  MWLottiePlugin
+//
+//  Created by Xavi Moll on 23/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MWLottiePlugin.
+FOUNDATION_EXPORT double MWLottiePluginVersionNumber;
+
+//! Project version string for MWLottiePlugin.
+FOUNDATION_EXPORT const unsigned char MWLottiePluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWLottiePlugin/PublicHeader.h>
+
+
