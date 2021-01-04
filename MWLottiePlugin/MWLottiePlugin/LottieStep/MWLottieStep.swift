c238c1c01fbca2be07cdfb46c850d5eddff87d81
Xavi Moll <xmollv@gmail.com>
2021-01-04 15:59:25 +0100

    Correctly loaded the animation

--- MWLottiePlugin/MWLottiePlugin/LottieStep/MWLottieStep.swift
+++ MWLottiePlugin/MWLottiePlugin/LottieStep/MWLottieStep.swift
@@ -10,16 +10,10 @@ import MobileWorkflowCore
 
 public class MWLottieStep: ORKStep {
     
-    // Required to be able to conform to RemoteContentStep down below
-    private let _services: MobileWorkflowServices
-    private let _authenticationWorkflowId: Int?
+    let remoteAnimationURL: URL
     
-    private var remoteAnimationURL: URL
-    
-    init(identifier: String, fileURL: URL, services: MobileWorkflowServices, authenticationWorkflowId: Int?) {
+    init(identifier: String, fileURL: URL) {
         self.remoteAnimationURL = fileURL
-        self._services = services
-        self._authenticationWorkflowId = authenticationWorkflowId
         super.init(identifier: identifier)
     }
     
@@ -35,56 +29,9 @@ public class MWLottieStep: ORKStep {
 extension MWLottieStep: MobileWorkflowStep {
     public static func build(step stepInfo: StepInfo, services: MobileWorkflowServices) throws -> ORKStep {
         if let urlString = stepInfo.data.content["lottieFileURL"] as? String, let url = URL(string: urlString) {
-            return MWLottieStep(identifier: stepInfo.data.identifier,
-                                fileURL: url,
-                                services: services,
-                                authenticationWorkflowId: stepInfo.data.content["authenticationWorkflowId"] as? Int)
+            return MWLottieStep(identifier: stepInfo.data.identifier, fileURL: url)
         } else {
             throw NSError(domain: "io.mobileworkflow.lottie", code: 0, userInfo: [NSLocalizedDescriptionKey:"URL to fetch the lottie file missing from the JSON"])
         }
     }
 }
-
-extension MWLottieStep: RemoteContentStep {
-    public typealias ResponseType = Data
-    
-    public var services: MobileWorkflowServices {
-        return self._services
-    }
-    
-    public var contentURL: String? {
-        return self.remoteAnimationURL.absoluteString
-    }
-    
-    public var authenticationWorkflowId: Int? {
-        return self._authenticationWorkflowId
-    }
-
-    public func loadContent(completion: @escaping (Result<Data, Error>) -> Void) {
-//        self.perform(url: self.remoteAnimationURL.absoluteString, method: .GET, completion: completion)
-        
-        #warning("This implementation is wrong, it bypasses the system. For some reason it doesn't seem to be able to decode the JSON as Data")
-        let task = URLSession.shared.dataTask(with: URLRequest(url: self.remoteAnimationURL)) { (dataOrNil, responseOrNil, errorOrNil) in
-            if let error = errorOrNil {
-                completion(.failure(error))
-            } else if let data = dataOrNil {
-                completion(.success(data))
-            } else {
-                assertionFailure("No error nor data")
-            }
-        }
-        task.resume()
-    }
-}
-
-extension MWLottieStep: SyncableContentSource {
-    public var resolvedURL: URL? {
-        get {
-            return self.remoteAnimationURL
-        }
-        set {
-            self.remoteAnimationURL = newValue ?? self.remoteAnimationURL
-        }
-    }
-}
-
--- MWLottiePlugin/MWLottiePlugin/LottieStep/MWLottieViewController.swift
+++ MWLottiePlugin/MWLottiePlugin/LottieStep/MWLottieViewController.swift
@@ -9,30 +9,34 @@ import Lottie
 import Foundation
 import MobileWorkflowCore
 
-public class MWLottieViewController: ORKStepViewController, RemoteContentStepViewController, ContentClearable {
+public class MWLottieViewController: ORKStepViewController {
     
-    public typealias StepType = MWLottieStep
-    public var remoteContentStep: MWLottieStep! { return (self.step as! MWLottieStep) }
-    public var workflowPresentationDelegate: WorkflowPresentationDelegate?
+    private let animationView = AnimationView()
+    private var lottieStep: MWLottieStep {
+        guard let mapStep = self.step as? MWLottieStep else {
+            preconditionFailure("Unexpected step type. Expecting \(String(describing: MWLottieStep.self)), got \(String(describing: type(of: self.step)))")
+        }
+        
+        return mapStep
+    }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        self.resyncContent()
-    }
-    
-    public func showLoading() {
         
-    }
-    
-    public func hideLoading() {
+        self.animationView.frame = self.view.bounds
+        self.animationView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(self.animationView)
         
-    }
-    
-    public func update(content: Data) {
-        dump(content)
-    }
-    
-    public func clearContent() {
+        Animation.loadedFrom(url: self.lottieStep.remoteAnimationURL, closure: { [weak self] animationOrNil in
+            guard let animation = animationOrNil else {
+                assertionFailure("Failed to load animation")
+                return
+            }
+            
+            self?.animationView.animation = animation
+            self?.animationView.loopMode = .loop
+            self?.animationView.play()
+        }, animationCache: nil)
         
     }
 }
