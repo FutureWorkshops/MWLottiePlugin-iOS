6ed2a814895d934f5051524d6edd84873a30bfe6
Xavi Moll <xmollv@gmail.com>
2021-01-04 13:08:32 +0100

    Added the conformance to RemoteContentStep

--- MWLottiePlugin/MWLottiePlugin/LottieStep/MWLottieStep.swift
+++ MWLottiePlugin/MWLottiePlugin/LottieStep/MWLottieStep.swift
@@ -10,11 +10,16 @@ import MobileWorkflowCore
 
 public class MWLottieStep: ORKStep {
     
-    let remoteAnimationURL: URL
-    private(set) var localAnimationURL: URL?
+    // Required to be able to conform to RemoteContentStep down below
+    private let _services: MobileWorkflowServices
+    private let _authenticationWorkflowId: Int?
     
-    init(identifier: String, fileURL: URL) {
+    private let remoteAnimationURL: URL
+    
+    init(identifier: String, fileURL: URL, services: MobileWorkflowServices, authenticationWorkflowId: Int?) {
         self.remoteAnimationURL = fileURL
+        self._services = services
+        self._authenticationWorkflowId = authenticationWorkflowId
         super.init(identifier: identifier)
     }
     
@@ -30,10 +35,33 @@ public class MWLottieStep: ORKStep {
 extension MWLottieStep: MobileWorkflowStep {
     public static func build(step stepInfo: StepInfo, services: MobileWorkflowServices) throws -> ORKStep {
         if let urlString = stepInfo.data.content["lottieFileURL"] as? String, let url = URL(string: urlString) {
-            return MWLottieStep(identifier: stepInfo.data.identifier, fileURL: url)
+            return MWLottieStep(identifier: stepInfo.data.identifier,
+                                fileURL: url,
+                                services: services,
+                                authenticationWorkflowId: stepInfo.data.content["authenticationWorkflowId"] as? Int)
         } else {
             throw NSError(domain: "io.mobileworkflow.lottie", code: 0, userInfo: [NSLocalizedDescriptionKey:"URL to fetch the lottie file missing from the JSON"])
         }
+    }
+}
+
+extension MWLottieStep: RemoteContentStep {
+    public typealias ResponseType = Data
+    
+    public var services: MobileWorkflowServices {
+        return self._services
+    }
+    
+    public var contentURL: String? {
+        return self.remoteAnimationURL.absoluteString
+    }
+    
+    public var authenticationWorkflowId: Int? {
+        return self._authenticationWorkflowId
+    }
+
+    public func loadContent(completion: @escaping (Result<Data, Error>) -> Void) {
+        self.perform(url: self.remoteAnimationURL.absoluteString, method: .GET, completion: completion)
     }
 }
 
