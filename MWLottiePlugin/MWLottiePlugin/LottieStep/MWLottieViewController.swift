d1d1c855f83a6293ff6a8f9122c4533572e3efb4
Jonathan Flintham <jonathan@futureworkshops.com>
2021-05-11 11:42:16 +0100

    Now using MWInstructionStepViewController

--- MWLottiePlugin/MWLottiePlugin/LottieStep/MWLottieViewController.swift
+++ MWLottiePlugin/MWLottiePlugin/LottieStep/MWLottieViewController.swift
@@ -9,33 +9,31 @@ import Lottie
 import Foundation
 import MobileWorkflowCore
 
-public class MWLottieViewController: ORKInstructionStepViewController {
+public class MWLottieViewController: MWInstructionStepViewController {
     
     //MARK: private properties
-    private var lottieStep: MWLottieStep { self.step as! MWLottieStep }
+    private var lottieStep: MWLottieStep { self.mwStep as! MWLottieStep }
     
     //MARK: Lifecycle
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let placeholderImageView = self.imageView {
-            let animationView = AnimationView()
-            animationView.frame = placeholderImageView.bounds
-            animationView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            placeholderImageView.addSubview(animationView)
+        let placeholderView: UIView = self.imageView
+        
+        let animationView = AnimationView()
+        animationView.frame = placeholderView.bounds
+        animationView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        placeholderView.addSubview(animationView)
+        
+        Animation.loadedFrom(url: self.lottieStep.remoteAnimationURL, closure: { animationOrNil in
+            guard let animation = animationOrNil else {
+                assertionFailure("Failed to load animation")
+                return
+            }
             
-            Animation.loadedFrom(url: self.lottieStep.remoteAnimationURL, closure: { animationOrNil in
-                guard let animation = animationOrNil else {
-                    assertionFailure("Failed to load animation")
-                    return
-                }
-                
-                animationView.animation = animation
-                animationView.loopMode = .loop
-                animationView.play()
-            }, animationCache: LRUAnimationCache.sharedCache)
-        } else {
-            assertionFailure("The lottie animation won't be shown because the imageView is missing and we don't know where to place it")
-        }
+            animationView.animation = animation
+            animationView.loopMode = .loop
+            animationView.play()
+        }, animationCache: LRUAnimationCache.sharedCache)
     }
 }
