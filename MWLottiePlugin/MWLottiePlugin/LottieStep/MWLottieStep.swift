28cf0cfe80edd183dba0f5e245d06b98640b2edc
Igor Ferreira <me@igorcferreira.dev>
2023-03-14 18:30:02 +0100

    Add metadata to Animated Instructions step

--- MWLottiePlugin/MWLottiePlugin/LottieStep/MWLottieStep.swift
+++ MWLottiePlugin/MWLottiePlugin/LottieStep/MWLottieStep.swift
@@ -54,3 +54,46 @@ extension MWLottieStep: BuildableStep {
         return step
     }
 }
+
+public class LottieAnimatedInstructionsMetadata: StepMetadata {
+    enum CodingKeys: CodingKey {
+        case text
+        case lottieFileURL
+    }
+    
+    let text: String?
+    let lottieFileURL: String?
+    
+    init(id: String, title: String, text: String?, lottieFileURL: String?, next: PushLinkMetadata?, links: [PushLinkMetadata]) {
+        self.text = text
+        self.lottieFileURL = lottieFileURL
+        super.init(id: id, type: "io.mobileworkflow.LottieInstructions", title: title, next: next, links: links)
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.text = try container.decodeIfPresent(String.self, forKey: .text)
+        self.lottieFileURL = try container.decodeIfPresent(String.self, forKey: .lottieFileURL)
+        try super.init(from: decoder)
+    }
+    
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeIfPresent(self.text, forKey: .text)
+        try container.encodeIfPresent(self.lottieFileURL, forKey: .lottieFileURL)
+        try super.encode(to: encoder)
+    }
+}
+
+public extension StepMetadata {
+    static func lottieAnimatedInstructions(
+        id: String,
+        title: String,
+        text: String? = nil,
+        lottieFileURL: String? = nil,
+        next: PushLinkMetadata? = nil,
+        links: [PushLinkMetadata] = []
+    ) -> LottieAnimatedInstructionsMetadata {
+        LottieAnimatedInstructionsMetadata(id: id, title: title, text: text, lottieFileURL: lottieFileURL, next: next, links: links)
+    }
+}
